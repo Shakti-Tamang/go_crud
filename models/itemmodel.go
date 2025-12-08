0616f39d72a4fd1eb2772e8ebd8564d26a179bf0
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2025-12-08 15:48:11 +0545

    feat: go model design

--- models/itemmodel.go
+++ models/itemmodel.go
@@ -1,1 +1,11 @@
 package models
+
+type Item struct {
+	Id uint `json:"id" gorm:"primaryKey;autoIncrement"`
+
+	ProductName string `json:"product_name"`
+
+	Description string `json:"description"`
+
+	Quantity int16 `json:"quantity"`
+}
