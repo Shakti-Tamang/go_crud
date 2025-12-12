0fab4ea4f7b6c29219fd0dcef1b36152cf886a2a
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2025-12-12 13:20:17 +0545

    setup go migration

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -1,1 +1,15 @@
-package migrate
+package main
+
+import (
+	"github.com/Shakti-Tamang/crud-app/initializers"
+	"github.com/Shakti-Tamang/crud-app/models"
+)
+
+func init() {
+	initializers.ConnectDB()
+	initializers.LoadEnvVariable()
+}
+
+func main() {
+	initializers.DB.AutoMigrate(&models.Item{})
+}
